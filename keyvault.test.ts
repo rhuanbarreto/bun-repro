ab9a8fe099c99a782b96bc086f00bec3e2ae9fee
Rhuan Barreto <rhuan@rhuan.com.br>
2024-06-02 21:08:20 +0200

    chore: Update timeout minutes in pull request workflow

--- keyvault.test.ts
+++ keyvault.test.ts
@@ -2,11 +2,22 @@ import { describe, it, expect } from "bun:test";
 import { DefaultAzureCredential } from "@azure/identity";
 import { SecretClient } from "@azure/keyvault-secrets";
 
+const credential = new DefaultAzureCredential({
+  additionallyAllowedTenants: ["*"],
+});
+
 describe("always passing test", () => {
   it("should pass", async () => {
-    const credential = new DefaultAzureCredential({
-      additionallyAllowedTenants: ["*"],
-    });
+    const kvURL = process.env.KEYVAULT_URL;
+    if (!kvURL) throw new Error("KEYVAULT_URL is not set");
+    const client = new SecretClient(kvURL, credential);
+    const secret = await client.getSecret("bun-secret");
+    expect(secret).not.toBeNull();
+  });
+});
+
+describe("always passing test 2", () => {
+  it("should pass", async () => {
     const kvURL = process.env.KEYVAULT_URL;
     if (!kvURL) throw new Error("KEYVAULT_URL is not set");
     const client = new SecretClient(kvURL, credential);
