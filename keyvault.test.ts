eb0ace95fee5493e5955014471b17817e4cc5b0e
Rhuan Barreto <rhuan@rhuan.com.br>
2024-06-03 22:25:17 +0200

    force a failure

--- keyvault.test.ts
+++ keyvault.test.ts
@@ -5,23 +5,19 @@ import { SecretClient } from "@azure/keyvault-secrets";
 const credential = new DefaultAzureCredential({
   additionallyAllowedTenants: ["*"],
 });
+const kvURL = process.env.KEYVAULT_URL;
+if (!kvURL) throw new Error("KEYVAULT_URL is not set");
+const client = new SecretClient(kvURL, credential);
 
 describe("always passing test", () => {
   it("should pass", async () => {
-    const kvURL = process.env.KEYVAULT_URL;
-    if (!kvURL) throw new Error("KEYVAULT_URL is not set");
-    const client = new SecretClient(kvURL, credential);
-    const secret = await client.getSecret("bun-secret");
-    expect(secret).not.toBeNull();
-  });
-});
-
-describe("always passing test 2", () => {
-  it("should pass", async () => {
-    const kvURL = process.env.KEYVAULT_URL;
-    if (!kvURL) throw new Error("KEYVAULT_URL is not set");
-    const client = new SecretClient(kvURL, credential);
-    const secret = await client.getSecret("bun-secret");
-    expect(secret).not.toBeNull();
+    await Promise.all(
+      Array(10)
+        .fill(0)
+        .map(async () => {
+          const secret = await client.getSecret("bun-secret");
+          expect(secret).not.toBeNull();
+        })
+    );
   });
 });
